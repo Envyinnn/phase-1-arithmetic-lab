3c3990109129e8837ecb2a8fe8968ca2a2bc1dfc
Dan <daweidern@gmail.com>
2022-06-28 11:54:53 -0500

    commit tree 71f3e3ea5b7b0638b6a3cf69a29df1d64a78fdcb

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+var num1 = 31
+var num2 = 2
+var multiply = num1*num2
+    multiply;
 
-// Write your code here
+ 
+var random = 1
+ console.log(Math.random());
+
+var num3 = 10
+var num4 = 6
+
+var mod = num3%num4
+    mod;
+
+var max = 20
+console.log(math.max(5,10,15,20));
